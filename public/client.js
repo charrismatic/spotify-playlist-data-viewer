c06ec278ff4ef6593a55da57d4aca6e670f0131f
Glitch (spotify-playlist-data-viewer) <none>
2019-08-10 21:25:49 +0000

    🌟🍦 Checkpoint ./public/client.js:5157936/12720

--- public/client.js
+++ public/client.js
@@ -3,7 +3,6 @@ function drawFeatures(data) {
   
 }
 
-
 function getFeatures(id) {
   let query = '/features?id=' + id;
   $.get(query, function(data) {
@@ -99,10 +98,14 @@ function getFeatures(id) {
 }
 
 
-function drawAnalysis(data) {
+function drawAnalysis(_track) {
   console.log('drawAnalysis');
+  
+  let id = _track.id;
+  let data = _track.data; 
+  
   console.log(data);
-
+  
   let deviceId = '';
   var img = new Image;
   
@@ -117,107 +120,164 @@ function drawAnalysis(data) {
     'rgba(175,40,150, 0.9)',
     'rgba(30,50,100, 0.9)'
   ]
- data.
-  const featuresChart = document.getElementById('features-chart');
-  featuresChart.style.width = featuresChart.offsetWidth;
-  featuresChart.width = featuresChart.offsetWidth * 2;
-  featuresChart.style.height = featuresChart.offsetHeight;
-  featuresChart.height = featuresChart.offsetHeight * 2;
- 
-  const width = featuresChart.width;
-  const height = featuresChart.height;
-  const ctx = featuresChart.getContext("2d");
-
+  
+  function binaryIndexOf(searchElement, valueof, valueout) {
+    var minIndex = 0;
+    var maxIndex = this.length - 1;
+    var currentIndex;
+    var currentElement;
+    while (minIndex <= maxIndex) {
+      currentIndex = (minIndex + maxIndex) / 2 | 0;
+      currentElement = valueof(this[currentIndex]);
+      if (currentElement < searchElement && ((currentIndex + 1 < this.length) 
+      ? valueof(this[currentIndex+1]) : Infinity) > searchElement) {
+        return valueout(currentElement, currentIndex, this);
+      }
+      
+      if (currentElement < searchElement) {
+        minIndex = currentIndex + 1;
+      } else if (currentElement > searchElement) {
+        maxIndex = currentIndex - 1;
+      } else { 
+        return this[currentIndex]; 
+      }
+    }
+    return -1;
+  }
+  
+  function analysisChartClickHanlder(clickEvent) {
+    var chart = clickEvent.target;
+    const time = (clickEvent.offsetX / chart.width) * data.track.duration * 2;
+    const kind = getFloorRowPosition(clickEvent.offsetY * 2 , rowHeight);
+    const seekTime = binaryIndexOf.call(  
+      arrayLikes[kind], 
+      time, 
+      e => e.start, 
+      (element, index) => element
+    );
+    
+    fetch(`https://api.spotify.com/v1/me/player/seek?position_ms=${Math.floor(seekTime*1000)}`, {
+      method: "PUT",
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    }).catch(console.log);
+  }
+  
+  const getCurrentAndLastArrayLikes = (arrayLikes, time) => {
+    arrayLikes.map(arrayLike => {
+      binaryIndexOf.call(arrayLike, time, e => e.start, 
+        (element, index, array) => ([
+          array[index],
+          array[index > 0 ? index - 1 : 0]
+        ])
+      )
+    })
+  };
+  
+  const getRowPosition = index => index === 0 ? 0 : 1 / index + getRowPosition(index-1);
+  const getFloorRowPosition = ( searchPosition, rowHeight, i=0, max=5) => i > max 
+  ? max : searchPosition < (getRowPosition(i+1) * rowHeight) 
+  ? i : getFloorRowPosition(searchPosition, rowHeight, i+1, max);
+  
+  const analysisChart = document.getElementById(`#an-${id}.features-chart`);
+  
+  analysisChart.style.width = analysisChart.offsetWidth;
+  analysisChart.width = analysisChart.offsetWidth * 2;
+  analysisChart.style.height = analysisChart.offsetHeight;
+  analysisChart.height = analysisChart.offsetHeight * 2;
+  
+  const width = analysisChart.width;
+  const height = analysisChart.height;
+  const ctx = analysisChart.getContext("2d");
+  
   const arrayLikesEntries = Object.entries(data)
-    .filter(entry => entry[1] instanceof Array)
-    .sort((a, b) => a[1].length - b[1].length)
+  .filter(entry => entry[1] instanceof Array)
+  .sort((a, b) => a[1].length - b[1].length)
   
-  const arrayLikesKeys = arrayLikesEntries
-    .map(entry => entry[0]);
-  
-  const arrayLikes = arrayLikesEntries
-    .map(entry => entry[1]);
-  
+  const arrayLikesKeys = arrayLikesEntries.map(entry => entry[0]);
+  const arrayLikes = arrayLikesEntries.map(entry => entry[1]);
   const rowHeight = height / arrayLikes.length;
   
-  featuresChart.addEventListener('click', analysisChartClickHanlder);
-  
+  analysisChart.addEventListener('click', analysisChartClickHanlder);
   arrayLikes.forEach((arrayLike, arrayLikeIndex) => {
     const startY = getRowPosition(arrayLikeIndex) * rowHeight;
     const arrayLikeHeight = rowHeight / (arrayLikeIndex + 1);
     arrayLike.forEach((section, sectionIndex) => {
       ctx.fillStyle = colors[sectionIndex % colors.length];
       ctx.fillRect(section.start/data.track.duration*width,
-                   getRowPosition(arrayLikeIndex) * rowHeight,
-                   section.duration/data.track.duration*width,
-                   arrayLikeHeight);
-    });
-    const label = arrayLikesKeys[arrayLikeIndex].charAt(0).toUpperCase() + arrayLikesKeys[arrayLikeIndex].slice(1)
-    ctx.fillStyle = "#000";
-    ctx.font = `bold ${arrayLikeHeight}px Circular`;
-    ctx.fillText(label,0,startY + arrayLikeHeight);
-  });
-  const markerHeight = getRowPosition(arrayLikes.length) * rowHeight;
-  function provideAnimationFrame(timestamp) {
-    player && player.getCurrentState().then(state => {
-      ctx.clearRect(0, 0, featuresChart.width, featuresChart.height);
-      ctx.drawImage(img,0,0);
+        getRowPosition(arrayLikeIndex) * rowHeight,
+        section.duration/data.track.duration*width,
+        arrayLikeHeight);
+      });
+      const label = arrayLikesKeys[arrayLikeIndex].charAt(0).toUpperCase() + arrayLikesKeys[arrayLikeIndex].slice(1)
       ctx.fillStyle = "#000";
-
-      const position = state.position/1000/data.track.duration*width
-      ctx.fillRect(
-          position-2,
+      ctx.font = `bold ${arrayLikeHeight}px Circular`;
+      ctx.fillText(label,0,startY + arrayLikeHeight);
+    });
+    
+    const markerHeight = getRowPosition(arrayLikes.length) * rowHeight;
+    function provideAnimationFrame(timestamp) {
+      player && player.getCurrentState().then(state => {
+        ctx.clearRect(0, 0, analysisChart.width, analysisChart.height);
+        ctx.drawImage(img,0,0);
+        ctx.fillStyle = "#000";
+        
+        const position = state.position/1000/data.track.duration*width
+        ctx.fillRect(position-2,
           0,
           5,
-          markerHeight
-      );
-      
-      
+          markerHeight);
+          
+          
+          
+          const currentAndLastArrayLikes = getCurrentAndLastArrayLikes(arrayLikes, state.position/1000);
+          const pitchChanges = currentAndLastArrayLikes[3][0].pitches.map((pitch, index) => Math.abs(pitch - currentAndLastArrayLikes[3][1].pitches[index]));
+          const timbreChanges = currentAndLastArrayLikes[3][0].timbre.map((timbre, index) => Math.abs(timbre - currentAndLastArrayLikes[3][1].timbre[index]));
+          
+          // Pitch boxes
+          const pitchBoxWidth = 60;
+          ctx.strokeStyle = "#AAA";
+          pitchChanges.forEach((pitchChange, i) => {
+            ctx.fillStyle = `hsl(0, 0%, ${pitchChange * 100}%)`;
+            ctx.fillRect(i*pitchBoxWidth,
+              height - 2 * pitchBoxWidth,
+              pitchBoxWidth,
+              pitchBoxWidth);
+            });
 
-      const currentAndLastArrayLikes = getCurrentAndLastArrayLikes(arrayLikes, state.position/1000);
-      const pitchChanges = currentAndLastArrayLikes[3][0].pitches.map((pitch, index) => Math.abs(pitch - currentAndLastArrayLikes[3][1].pitches[index]));
-      const timbreChanges = currentAndLastArrayLikes[3][0].timbre.map((timbre, index) => Math.abs(timbre - currentAndLastArrayLikes[3][1].timbre[index]));
-      
-      // Pitch boxes
-      const pitchBoxWidth = 60;
-      ctx.strokeStyle = "#AAA";
-      pitchChanges.forEach((pitchChange, i) => {
-        ctx.fillStyle = `hsl(0, 0%, ${pitchChange * 100}%)`;
-        ctx.fillRect(i*pitchBoxWidth,
-                     height - 2 * pitchBoxWidth,
-                     pitchBoxWidth,
-                     pitchBoxWidth);
-      });
-      timbreChanges.forEach((timbreChange, i) => {
-        ctx.fillStyle = `hsl(0, 0%, ${timbreChange * 100}%)`;
-        ctx.fillRect(i*pitchBoxWidth,
-                     height - 4 * pitchBoxWidth,
-                     pitchBoxWidth,
-                     pitchBoxWidth);
-      });
-      currentAndLastArrayLikes[3][0].pitches.forEach((pitchChange, i) => {
-        ctx.fillStyle = `hsl(0, 0%, ${pitchChange * 100}%)`;
-        ctx.fillRect(i*pitchBoxWidth,
-                     height - pitchBoxWidth,
-                     pitchBoxWidth,
-                     pitchBoxWidth);
-      });
-      currentAndLastArrayLikes[3][0].timbre.forEach((pitchChange, i) => {
-        ctx.fillStyle = `hsl(0, 0%, ${pitchChange * 100}%)`;
-        ctx.fillRect(i*pitchBoxWidth,
-                     height - 3 * pitchBoxWidth,
-                     pitchBoxWidth,
-                     pitchBoxWidth);
-      });
-      
-      window.requestAnimationFrame(provideAnimationFrame);
-    }).catch(e => {
-      console.error("Animation: ", e);
-      window.requestAnimationFrame(provideAnimationFrame);
-    });
-  }
-  window.requestAnimationFrame(provideAnimationFrame);
-  img.src = featuresChart.toDataURL('png');
+            timbreChanges.forEach((timbreChange, i) => {
+              ctx.fillStyle = `hsl(0, 0%, ${timbreChange * 100}%)`;
+              ctx.fillRect(i*pitchBoxWidth,
+                height - 4 * pitchBoxWidth,
+                pitchBoxWidth,
+                pitchBoxWidth);
+              });
+
+              currentAndLastArrayLikes[3][0].pitches.forEach((pitchChange, i) => {
+                ctx.fillStyle = `hsl(0, 0%, ${pitchChange * 100}%)`;
+                ctx.fillRect(i*pitchBoxWidth,
+                  height - pitchBoxWidth,
+                  pitchBoxWidth,
+                  pitchBoxWidth);
+                });
+
+                currentAndLastArrayLikes[3][0].timbre.forEach((pitchChange, i) => {
+                  ctx.fillStyle = `hsl(0, 0%, ${pitchChange * 100}%)`;
+                  ctx.fillRect(i*pitchBoxWidth,
+                    height - 3 * pitchBoxWidth,
+                    pitchBoxWidth,
+                    pitchBoxWidth);
+                  });
+                  
+              window.requestAnimationFrame(provideAnimationFrame);
+            }).catch(e => {
+              console.error("Animation: ", e);
+              window.requestAnimationFrame(provideAnimationFrame);
+            });
+          }
+          window.requestAnimationFrame(provideAnimationFrame);
+          img.src = analysisChart.toDataURL('png');
 }
 
 
@@ -227,16 +287,15 @@ function getAnalysis(id) {
   return fetch(query).then(e => e.json()).then(_data => {
     drawAnalysis({
       data: _data,
-    }  
-
-                  );
-      // fetch(`https://api.spotify.com/v1/me/player/play${deviceId && `?device_id=${deviceId}`}`, {
-      //     method: "PUT",
-      //     body: JSON.stringify({"uris": [`spotify:track:${id}`]}),
-      //     headers: {
-      //       'Authorization': `Bearer ${accessToken}`
-      //     }
-      //   }).catch(e => console.error(e));
+      id: id,
+    });
+    // fetch(`https://api.spotify.com/v1/me/player/play${deviceId && `?device_id=${deviceId}`}`, {
+    //     method: "PUT",
+    //     body: JSON.stringify({"uris": [`spotify:track:${id}`]}),
+    //     headers: {
+    //       'Authorization': `Bearer ${accessToken}`
+    //     }
+    //   }).catch(e => console.error(e));
   });
 }
 
@@ -307,13 +366,8 @@ $.get('/playlists-tracks', function(data) {
 
 
 $.get('/analysis', function(data) {
-  var keys = ["danceability", "energy", "acousticness", "tempo", "instrumentalness"]
-  keys.map(function(key, i) {
-    if (data.hasOwnProperty(key)) {
-      var feature = $('<p style="#ffffff"><span class="big-number">' + data[key] + ' </span>'  + key + '</p>');
-      feature.appendTo('#audio-features-container');
-    }
-  });
+
+
 });
 
 $.get('/features', function(data) {
@@ -326,24 +380,21 @@ $.get('/features', function(data) {
   });
 });
 
+
 $.get('/artist', function(data) {
   var img = $('<img class="circle-image" />');
   img.attr('src', data.images[0].url);
   img.appendTo('#artist-container');
 
-  // Display the artist name
   var trackName = $('<h3>' + data.name + '</h3>');
   trackName.appendTo('#artist-container');
 
-  // Display the artist's popularity
   var trackName = $('<h3>Popularity: ' + data.popularity + '</h3>');
   trackName.appendTo('#artist-container');
 
-  // Display the artist's follower count
   var trackName = $('<h3>Followers: ' + data.followers.total + '</h3>');
   trackName.appendTo('#artist-container');
 
-  // Display the artist's genres
   data.genres.map(function(genre, i) {
     var genreItem = $('<p>' + genre + '</p>');
     genreItem.appendTo('#artist-container');
@@ -351,12 +402,6 @@ $.get('/artist', function(data) {
 });
 
 $.get('/artist-top-tracks', function(data) {
-  // "Data" is the object we get from the API. See server.js for the function that returns it.
-  console.group('%cResponse from /artist-top-tracks', 'color: #F037A5; font-size: large');
-  console.log(data);
-  console.groupEnd();
-
-  // Display the audio features
   data.map(function(track, i) {
     var trackName = $('<li>' + track.name + '</li>');
     trackName.appendTo('#top-tracks-container');
@@ -374,21 +419,11 @@ $.get('/category-playlists', function(data) {
 
 
 $.get('/user', function(data) {
-  // "Data" is the object we get from the API. See server.js for the function that returns it.
-  console.group('%cResponse from /user', 'color: #F037A5; font-size: large');
-  console.log(data);
-  console.groupEnd();
-
-  // Display the user's image
   var img = $('<div class="circle-image"></div>');
   img.attr('style', 'background:url(' + data.images[0].url + ') center/cover;height:300px;width:300px;');
   img.appendTo('#user-container');
-
-  // Display the user id
   var trackName = $('<a href="' + data.external_urls.spotify + '" style="color:white;text-decoration:underline;"><h3>' + data.id + '</h3></a>');
   trackName.appendTo('#user-container');
-
-  // Display the user's follower count
   var trackName = $('<h3>Followers: ' + data.followers.total + '</h3>');
   trackName.appendTo('#user-container');
 });
